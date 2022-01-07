3c3f0951f0e346a987bee948d2a00f38b666a580
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-07 18:34:54 +0530

    move-copy code

--- move-copy file.bat
+++ move-copy file.bat
@@ -1,4 +1,5 @@
 @echo off
-REM "copy/xcopy>sue for copy" "move>use for move" "1st source 2nd destination"
+REM "copy/xcopy>use for copy" "move>use for move" "1st source 2nd destination"
+REM "call > use for view copy/move data"
 xcopy d:\sad.jpg c:\users\subhojit\desktop
 call c:\users\subhojit\desktop\sad.jpg
