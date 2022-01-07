d0f848b039f8056107fb4e2c5d19d97c08d713b8
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-07 18:28:58 +0530

    loding anemation

---
+++ loding screen.bat
@@ -0,0 +1,93 @@
+@echo off
+title Loding
+mode 70
+color 02
+echo loding.
+timeout /t 1 > nul
+cls
+echo loding..
+timeout /t 1 > nul
+cls
+echo loding...
+timeout /t 1 > nul
+cls
+echo loding....
+timeout /t 1 > nul
+cls
+echo loding.....
+timeout /t 1 > nul
+cls
+echo loding......
+timeout /t 1 > nul
+cls
+echo loding.......
+timeout /t 1 > nul
+cls
+
+echo loding.
+timeout /t 1 > nul
+cls
+echo loding..
+timeout /t 1 > nul
+cls
+echo loding...
+timeout /t 1 > nul
+cls
+echo loding....
+timeout /t 1 > nul
+cls
+echo loding.....
+timeout /t 1 > nul
+cls
+echo loding......
+timeout /t 1 > nul
+cls
+echo loding.......
+timeout /t 1 > nul
+cls
+
+
+echo loding.
+timeout /t 1 > nul
+cls
+echo loding..
+timeout /t 1 > nul
+cls
+echo loding...
+timeout /t 1 > nul
+cls
+echo loding....
+timeout /t 1 > nul
+cls
+echo loding.....
+timeout /t 1 > nul
+cls
+echo loding......
+timeout /t 1 > nul
+cls
+echo loding.......
+timeout /t 1 > nul
+cls
+
+echo loding.
+timeout /t 1 > nul
+cls
+echo loding..
+timeout /t 1 > nul
+cls
+echo loding...
+timeout /t 1 > nul
+cls
+echo loding....
+timeout /t 1 > nul
+cls
+echo loding.....
+timeout /t 1 > nul
+cls
+echo loding......
+timeout /t 1 > nul
+cls
+echo loding.......
+timeout /t 1 > nul
+cls
+pause >nul
