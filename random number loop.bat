919219492cb31154eeb575816800d84a0c1ae8d2
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-07 18:37:16 +0530

    like hacker attack screen

---
+++ random number loop.bat
@@ -0,0 +1,6 @@
+@echo off
+color 01
+goto a
+:a
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%%random% %random% %random% %random% %random% %random% %random% %random% 
+goto a
