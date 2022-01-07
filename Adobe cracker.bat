aa1a06ef95c0d03b1aab65508e6c4f2c270690f9
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-07 18:24:39 +0530

    Cracker create

---
+++ Adobe cracker.bat
@@ -0,0 +1,30 @@
+@echo off
+title  :Adobe Cracker: 
+color 02
+timeout /t 5 /nobreak
+goto 1
+:1
+echo subhjit koley
+echo.
+echo (1)Adobe Photoshop 5.5 (2) Adobe PageMaker 6.5 (3) Adobe Pagemaker 7.0 (4) Adobe Photoshop 7.0 (5) Adobe Photoshop CS2
+echo (6) Abbyy Finereader 6.0 Professional (7) Adobe Acrobat 4.0 (8) Adobe After Effects 4.1 (9) Adobe Creative Swite 3 Master Collection 
+echo(10) adobe photoshop CS2 
+echo.
+set /p soft=chose a soft number? =
+if %soft%==1 echo  PWE550R5432199-540 
+if %soft%==2 echo  03W601R1114316-716 
+if %soft%==3 echo   1039-1121-2998-7586-7388-7545 
+if %soft%==exit exit
+if %soft%==4 echo  1045-1209-6738-4668-7696-2783 
+if %soft%==5 echo  1045-1081-2242-7754-2137-8480 
+if %soft%==6 echo  FPEF-6025-9000-2854-9821 
+if %soft%==7 echo  KWW400R7181499-945
+if %soft%==8 echo  EWW400R4000123-666-922
+if %soft%==9 echo  1325-1990-7497-1950-0519-7515
+if %soft%==10 echo 1/0 
+if %soft%==11 echo 
+if %soft%==12 echo
+pause > nul
+cls
+goto 1
+pause > nul
